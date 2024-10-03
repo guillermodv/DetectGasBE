826efc40866cdd17a6889487ce25df3b6a876141
guillermo.dv <guillermo.dv@gmail.com>
2024-10-03 19:53:34 -0300

    fix: change id types

--- models/core.model.js
+++ models/core.model.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, Sequelize) => {
   const Core = sequelize.define("core", {
     userId: {
-      type: Sequelize.STRING,
+      type: Sequelize.INTEGER,
       allowNull: false,
-    },    
+    },
     measurement: {
       type: Sequelize.STRING,
       allowNull: false,
--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,13 @@
 module.exports = (sequelize, Sequelize) => {
   const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     name: {
       type: Sequelize.STRING,
-    },    
+    },
     email: {
       type: Sequelize.STRING,
     },
